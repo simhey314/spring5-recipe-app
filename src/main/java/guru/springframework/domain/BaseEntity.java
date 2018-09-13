75e6146845b72d2072742ad795329547d6c6ed21
Simon Heyden <simon@family-heyden.net>
2018-09-14 00:47:08 +0200

    fixed BaseEntity inheritance issue
    - add annotation for mapped super class

--- src/main/java/guru/springframework/domain/BaseEntity.java
+++ src/main/java/guru/springframework/domain/BaseEntity.java
@@ -1,11 +1,11 @@
 package guru.springframework.domain;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
-@Entity
+@MappedSuperclass
 public abstract class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
