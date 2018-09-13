f05fb02c3da7034c168ff21d5612fdc801956dac
Simon Heyden <simon@family-heyden.net>
2018-09-14 00:54:11 +0200

    fixed sql column naming convention
    - no camel case for sql columns

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO category (categoryName) VALUES ('American');
-INSERT INTO category (categoryName) VALUES ('Italian');
-INSERT INTO category (categoryName) VALUES ('Mexican');
-INSERT INTO category (categoryName) VALUES ('Fast Food');
+INSERT INTO category (category_name) VALUES ('American');
+INSERT INTO category (category_name) VALUES ('Italian');
+INSERT INTO category (category_name) VALUES ('Mexican');
+INSERT INTO category (category_name) VALUES ('Fast Food');
 INSERT INTO unit_of_measure (description) VALUES ('Tasse');
 INSERT INTO unit_of_measure (description) VALUES ('Esslöffel');
 INSERT INTO unit_of_measure (description) VALUES ('Teelöffel');
