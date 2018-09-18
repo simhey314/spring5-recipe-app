8c7caab1b3161da846d92e43b20bdd92e738e4f3
Simon Heyden <simon@family-heyden.net>
2018-09-18 19:07:09 +0200

    new recipes, some layout etc.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,10 +20,22 @@ public class RecipeController {
 		this.recipeService = recioeService;
 	}
 
-	@GetMapping("detail/{id}")
+	@GetMapping("/{id}/detail")
 	String detailById(@PathVariable final Long id, final Model model) {
 		final Recipe data = recipeService.findRecipeById(id);
 		model.addAttribute("recipe", data);
 		return "recipe/detail";
+	}
+
+	@GetMapping("/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/edit";
+	}
+
+	@PostMapping("/save")
+	public String saveRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+		RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+		return "redirect:/recipe/" + savedRecipe.getId() + "/detail/";
 	}
 }
